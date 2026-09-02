d3a223c1228c93acbf367e5853b2bf7a88baadb3
RionRei <reign.tifa.cacho@gmail.com>
2026-09-03 00:17:12 +0800

    Add files via upload

---
+++ Assignment3 (1).java
@@ -0,0 +1,20 @@
+public class Assignment3 { // Creates a class named Assignment3
+
+    public static void main(String[] args) { // Starts the main part of the program.
+
+        String firstName = "Reign Tifa"; // Store the user's firstname
+        String lastName = "Cacho"; // Store the user's lastname
+        String address = "Camiling Tarlac"; // Store the user's address
+        String mobileNumber = "09662364779"; // for storing the user's mobile number
+        int age = 20; // for storing user's age
+        double gwa = 90.13; // for storing user's GWA
+
+        System.out.println("First Name: " + firstName); // + joins the text and firstName, then displays them.
+        System.out.println("Last Name: " + lastName); // + joins the text and lastName, then displays them.
+        System.out.println("Address: " + address); // + joins the text and address, then displays them.
+        System.out.println("Mobile Number: " + mobileNumber); // + joins the text and mobileNumber, then displays them.
+        System.out.println("Age: " + age); // + joins the text and age, then displays them.
+        System.out.println("GWA: " + gwa); // + joins the text and gwa, then displays them.
+
+    } // Ends the main method.
+} // Ends the Assignment3 class.
